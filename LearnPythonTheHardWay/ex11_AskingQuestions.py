e9fec3fe4d23ea7ce911be55f4b905ce7f9e183d
Robert85 <rkrall@agribank.com>
2017-10-25 14:48:46 -0500

    prompting the user.

--- LearnPythonTheHardWay/ex11_AskingQuestions.py
+++ LearnPythonTheHardWay/ex11_AskingQuestions.py
@@ -2,5 +2,9 @@
 
 print("How old are you?")
 age = input()
+print("how tall are you?")
+height = input()
+print("how much do you weight?")
+weight = input()
 
-print(age)
+print("So, you're {0} years old, {1} tall and {2} heavy".format(age, height, weight))
