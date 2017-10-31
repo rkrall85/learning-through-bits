1c9b22e97ee66af18cbf93623d1dd09e0ace57a0
Robert85 <rkrall@agribank.com>
2017-10-31 13:30:22 -0500

    while loops and lists.

---
+++ LearnPythonTheHardWay/ex33_WhileLoops.py
@@ -0,0 +1,20 @@
+
+
+
+# ex 33 while loops
+
+i = 0
+numbers = []
+
+while i < 6:
+    print("At the top i is {}".format(i))
+    numbers.append(i)
+
+    i += 1
+    print("Numbers now: ", numbers)
+    print("At the bottom i is {}".format(i))
+
+print("The numbers:")
+
+for num in numbers:
+    print(num)
