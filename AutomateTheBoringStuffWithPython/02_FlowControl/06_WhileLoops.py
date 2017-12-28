02d8508a4eae6657faf2001ee4ba4b4d70b9c572
Robert85 <rkrall@agribank.com>
2017-12-28 13:31:01 -0600

    done with lession.

--- AutomateTheBoringStuffWithPython/02_FlowControl/06_WhileLoops.py
+++ AutomateTheBoringStuffWithPython/02_FlowControl/06_WhileLoops.py
@@ -8,7 +8,18 @@ while spam < 5:
 
 
 name = ''
-while name != 'your name':
+while True:
     print('please type your name')
     name = input()
+    if name == 'your name':
+        break
 print('Thank you')
+
+
+
+spam = 0
+while spam < 5:
+    spam = spam + 1
+    if spam == 3: #skip the print of spam of 3
+        continue
+    print ('spam is ' + str(spam))
