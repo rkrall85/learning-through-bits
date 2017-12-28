808c0224bb4975cdeb667a2ecfd0a2adf1d62755
Robert85 <rkrall@agribank.com>
2017-12-28 09:19:28 -0600

    bad file name.

---
+++ AutomateTheBoringStuffWithPython/02_FlowControl/06_WhileLoops.py
@@ -0,0 +1,14 @@
+
+
+spam = 1
+while spam < 5:
+    print('Hello world!')
+    spam = spam + 1
+
+
+
+name = ''
+while name != 'your name':
+    print('please type your name')
+    name = input()
+print('Thank you')
