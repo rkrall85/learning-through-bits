a5a2bab5e226558a55d6f10a5b7fbe793b0d2c50
Robert85 <rkrall@agribank.com>
2018-02-05 17:42:30 -0600

    moved folder structure.

---
+++ SEIS603.Python/HW-1/http2_1.py
@@ -0,0 +1,16 @@
+#Robert Krall
+#http2_1.py
+
+#outputing answer
+print(5 ** 2)           #25
+print(9 * 5)            #45
+print(15 / 12)          #1.25
+print(12 / 15)          #0.8
+print(15 // 12)         #1  #makes a int (truncate remainder)
+print(12 // 15)         #0
+print(5 % 2)            #1  #The % operator returns the remainder after division.
+print(9 % 5)            #4
+print(15 % 12)          #3
+print(12 % 15)          #12
+print(6 % 6)            #0
+print(0 % 7)            #0
