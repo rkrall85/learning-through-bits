8887627e9300eadab9d27a029e2da9c5cbddfd1d
Robert85 <rkrall@agribank.com>
2018-02-13 09:28:30 -0600

    hw 2 assign 1

---
+++ SEIS603.Python/HW-2/threelines.py
@@ -0,0 +1,51 @@
+#Robert Krall
+#threelines.py
+
+
+import turtle #import turtles library
+wn = turtle.Screen()
+wn.bgcolor("white")
+alex = turtle.Turtle()
+alex.color("black")
+alex.pensize(3)
+alex.speed(2)               #slowing down the turtle
+alex.setposition(0,0)       #set position at middle
+
+
+'''
+#create every action one by one.
+alex.left(90)               #putting angel north
+alex.forward(200)           #going north of 200 steps
+alex.penup()                #putting pen up
+alex.right(90)
+alex.forward(30)
+alex.right(90)              #putting south
+alex.pendown()
+alex.forward(200)           #going south
+alex.penup()                #putting tail up
+alex.left(90)
+alex.forward(30)
+alex.left(90)
+alex.pendown()
+alex.forward(200)
+'''
+
+#Creating all three lines via a loop.
+for i in range(4):
+    if i % 2 == 0: alex.left(90)
+    else: alex.right(90)
+    alex.forward(200)
+    alex.right(90)
+    alex.penup()
+    #determining which lat and long line to "draw"
+    if i == 0: alex.forward(30)
+    elif i == 1: alex.forward(30)
+    elif i == 2: alex.forward(60)
+    else: alex.forward(60)
+    #route back to going north for last box
+    if i == 1: alex.right(180)
+    alex.pendown()
+
+alex.penup()
+alex.setposition(0,0)
+wn.exitonclick()        #only exit on click
