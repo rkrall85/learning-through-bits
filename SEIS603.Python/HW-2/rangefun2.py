8fffeae39066bde2d116106af947a25abd45b0b3
Robert85 <rkrall@agribank.com>
2018-02-13 11:49:46 -0600

    hw 2 assign 2

---
+++ SEIS603.Python/HW-2/rangefun2.py
@@ -0,0 +1,28 @@
+#Robert Krall
+#rangefun2.py
+
+
+
+#Loop 1: 1 2 3 4 5 6 7 8 9 10
+for num in range(1,11,1): print(num, end=' ')
+print('')
+
+#Loop 2: 1 3 5 7 9
+for num in range(1,10,2): print(num, end=' ')
+print('')
+
+#Loop 3: 47 1
+for num in range(47,0,-46): print(num, end=' ')
+print('')
+
+#Loop 4: 10 9 8 7 6 5 4 3 2 1
+for num in range(10,0,-1): print(num, end=' ')
+print('')
+
+#Loop 5: 10 7 4 1
+for num in range(10,0,-3): print(num, end=' ')
+print('')
+
+#Loop 6: 1 5 9 13 17 21
+for num in range(1,22,4): print(num, end=' ')
+print('')
