6d1b94573f9b69304f0b6d69104e3d49dd80c298
Robert85 <rkrall@agribank.com>
2018-02-21 09:09:39 -0600

    another way to import functions in another file.

--- SEIS603.Python/homework/HW-3/mymain.py
+++ SEIS603.Python/homework/HW-3/mymain.py
@@ -2,18 +2,19 @@
 #mymain.py
 
 
-from temperatureConverter import celsiusToFahrenheit, fahrenheitToCelsius
+#from temperatureConverter import celsiusToFahrenheit, fahrenheitToCelsius
+import temperatureConverter
 
 print("*"*100)
 
 user_fahrenheit = int(input("What is the fahrenheit degree?"))
-c = fahrenheitToCelsius(user_fahrenheit)
+c = temperatureConverter.fahrenheitToCelsius(user_fahrenheit)
 print(str(user_fahrenheit) + " degrees fahrenheit converts to " + str(c) + " degrees celsius")
 
 print("*"*100)
 
 user_celsius  = int(input("What is the celsius temperature degree?"))
-f = celsiusToFahrenheit(user_celsius)
+f = temperatureConverter.celsiusToFahrenheit(user_celsius)
 print(str(user_celsius) + " degrees celsius converts to " + str(f) + " degrees fahrenheit")
 
 print("*"*100)
