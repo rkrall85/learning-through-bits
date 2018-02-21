c7bad93e04d840a754fed20d8081d7e840f78c90
Robert85 <rkrall@agribank.com>
2018-02-21 09:10:34 -0600

    giving import an allias.

--- SEIS603.Python/homework/HW-3/mymain.py
+++ SEIS603.Python/homework/HW-3/mymain.py
@@ -3,18 +3,18 @@
 
 
 #from temperatureConverter import celsiusToFahrenheit, fahrenheitToCelsius
-import temperatureConverter
+import temperatureConverter as tc
 
 print("*"*100)
 
 user_fahrenheit = int(input("What is the fahrenheit degree?"))
-c = temperatureConverter.fahrenheitToCelsius(user_fahrenheit)
+c = tc.fahrenheitToCelsius(user_fahrenheit)
 print(str(user_fahrenheit) + " degrees fahrenheit converts to " + str(c) + " degrees celsius")
 
 print("*"*100)
 
 user_celsius  = int(input("What is the celsius temperature degree?"))
-f = temperatureConverter.celsiusToFahrenheit(user_celsius)
+f = tc.celsiusToFahrenheit(user_celsius)
 print(str(user_celsius) + " degrees celsius converts to " + str(f) + " degrees fahrenheit")
 
 print("*"*100)
